75128019f499a09ea8b9cc1c38b64eb9ab8134b7
DeanWinchers <1048402892@qq.com>
2021-08-12 20:04:30 +0800

    add

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,6 +1,6 @@
 module.exports = {
-  name: 'William Shakespeare',
-  title: 'Poet, playwright and actor',
+  name: 'Yao Jianyu',
+  title: 'Master from ICT',
   facts: {
     'Residence': 'London',
     'Homepage': `<a href="https://en.wikipedia.org/wiki/William_Shakespeare">
@@ -34,7 +34,7 @@ Prince Hamlet is visited by his father's ghost and ordered to avenge his father'
     },
     {
       title: 'Twelfth Night',
-      written: '1600 – 1601',
+      written: '1600  1�7 1601',
       badges: ['comedy', 'love'],
       contents: `
 Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
@@ -42,7 +42,7 @@ Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebast
     },
     {
       title: 'The Tragedy of Romeo and Juliet',
-      written: '1595–1596, with a possible early draft written in 1591',
+      written: '1595 1�71596, with a possible early draft written in 1591',
       badges: ['tragedy', 'death', 'love'],
       contents: `
 In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
