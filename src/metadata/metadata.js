9321e15ea83ceecc6563680021794dbb74ce6d68
DeanWinchers <1048402892@qq.com>
2021-08-14 21:26:07 +0800

    add

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -7,9 +7,18 @@ module.exports = {
   plays: [
     {
       title: 'Small GEMM in Arm platform',
-      written: 'November 2020 - May 2021',
+      written: 'November 2020 - May 2021.   The paper is under review in ICPADS\'21',
       badges: ['C', 'Assembly', 'Makefile'],
-      contents: `GEMM with the small size of input matrices is becoming widely used in many fields like HPC and machine learning. Although many famous BLAS libraries already supported small GEMM, they cannot achieve near-optimal performance. This is because the costs of pack operations are high and frequent boundary processing cannot be neglected. This paper proposes an input-aware adaptive tuning framework(IAAT) for small GEMM to overcome the performance bottlenecks in state-of-the-art implementations. IAAT consists of two stages, the install-time stage and the run-time stage. In the run-time stage, IAAT tiles matrices into blocks to alleviate boundary processing. This stage utilizes an input-aware adaptive tile algorithm and plays the role of runtime tuning. In the install-time stage, IAAT auto-generates hundreds of kernels of different sizes to remove pack operations. Finally, IAAT finishes the computation of small GEMM by invoking different kernels, which corresponds to the size of blocks. The experimental results show that IAAT gains better performance than other BLAS libraries on ARMv8 platform.`
+      contents: `GEMM with the small size of input matrices is becoming widely used in many fields like HPC and machine learning. 
+      Although many famous BLAS libraries already supported small GEMM, they cannot achieve near-optimal performance. 
+      This is because the costs of pack operations are high and frequent boundary processing cannot be neglected. 
+      We proposes an input-aware adaptive tuning framework(IAAT) for small GEMM to overcome the performance bottlenecks in state-of-the-art implementations. 
+      IAAT consists of two stages, the install-time stage and the run-time stage. 
+      In the run-time stage, IAAT tiles matrices into blocks to alleviate boundary processing. 
+      This stage utilizes an input-aware adaptive tile algorithm and plays the role of runtime tuning. 
+      In the install-time stage, IAAT auto-generates hundreds of kernels of different sizes to remove pack operations. 
+      Finally, IAAT finishes the computation of small GEMM by invoking different kernels, which corresponds to the size of blocks. 
+      The experimental results show that IAAT gains better performance than other BLAS libraries on ARMv8 platform.`
     },
     {
       title: 'Small GEMM for cross platform',
@@ -18,10 +27,10 @@ module.exports = {
       contents: `IAAT for different platforms`
     },
     {
-      title: 'OpenVML: ',
+      title: 'OpenVML: Vector Math Library',
       written: 'August 2020 - April 2021',
       badges: ['C', 'Assembly', 'SIMD'],
-      contents: `Vector Math Library As a basic mathematical operation, the high-performance implementation of trigonometric functions is of great significance to the construction of the basic software ecology of the processor. Especially, the current processors have adopted the SIMD architecture, and the implementation of high-performance trigonometric functions based on SIMD has important research significance and application value. In this regard, the project uses numerical analysis method to implement and optimize the five commonly used trigonometric functions sin, cos, tan, atan, atan2 with high performance. Based on the analysis of floating-point IEEE754 standard, an efficient trigonometric function algorithm is designed. Then, the algorithm accuracy is further improved by the application of Taylor formula, Pade approximation and Remez algorithm in polynomial approximation algorithm. Finally, the performance of the algorithm is further improved by using instruction pipeline and SIMD optimization. The experimental results show that, on the premise of satisfying the accuracy, the trigonometric function implemented is compared with libm algorithm library and ARM_M algorithm library, on the ARM V8 computing platform, has achieved great performance improvement, which is 1.77-6.26 times higher than libm algorithm library, compared with ARM_M The library of M method is 1.34-1.5 times higher`
+      contents: `As a basic mathematical operation, the high-performance implementation of trigonometric functions is of great significance to the construction of the basic software ecology of the processor. Especially, the current processors have adopted the SIMD architecture, and the implementation of high-performance trigonometric functions based on SIMD has important research significance and application value. In this regard, the project uses numerical analysis method to implement and optimize the five commonly used trigonometric functions sin, cos, tan, atan, atan2 with high performance. Based on the analysis of floating-point IEEE754 standard, an efficient trigonometric function algorithm is designed. Then, the algorithm accuracy is further improved by the application of Taylor formula, Pade approximation and Remez algorithm in polynomial approximation algorithm. Finally, the performance of the algorithm is further improved by using instruction pipeline and SIMD optimization. The experimental results show that, on the premise of satisfying the accuracy, the trigonometric function implemented is compared with libm algorithm library and ARM_M algorithm library, on the ARM V8 computing platform, has achieved great performance improvement, which is 1.77-6.26 times higher than libm algorithm library, compared with ARM_M The library of M method is 1.34-1.5 times higher`
     },
   ],
   skills: [
