21637c004ef913aaadd5eb88c6910cbf0a540097
DeanWinchers <1048402892@qq.com>
2021-08-14 19:25:50 +0800

    add

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,27 +1,9 @@
 module.exports = {
   name: 'Yao Jianyu',
-  title: 'Master from ICT',
   facts: {
     'Residence': 'Beijing',
     'Email': '<a href="mailto:yaojianyu89@gmail.com">yaojianyu89@gmail.com</a>'
   },
-  skills: [
-    ['Comedy', '++++'],
-    ['Love', '++++'],
-    ['Theater', '++++'],
-    ['Tragedy', '++++'],
-    ['Poetry', '++++'],
-    ['Erotic', '+++'],
-    ['Sonnets', '++++'],
-    ['Plays', '++++'],
-    ['Writing', '++++'],
-    ['Directing', '+++'],
-    ['Literature', '++++'],
-    ['Influence', '++++'],
-    ['Influence', '++++'],
-    ['Influence', '++++'],
-    ['Influence', '++++'],
-  ],
   plays: [
     {
       title: 'Hamlet',
@@ -29,22 +11,6 @@ module.exports = {
       badges: ['tragedy', 'philosophy', 'death'],
       contents: `
 Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
-`
-    },
-    {
-      title: 'Twelfth Night',
-      written: '1600 鈥� 1601',
-      badges: ['comedy', 'love'],
-      contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
-`
-    },
-    {
-      title: 'The Tragedy of Romeo and Juliet',
-      written: '1595鈥�1596, with a possible early draft written in 1591',
-      badges: ['tragedy', 'death', 'love'],
-      contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
 `
     }
   ],
@@ -63,5 +29,22 @@ Thousands of performances of my plays have been staged since the end of the 16th
 In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. He dedicated them to Henry Wriothesley, Earl of Southampton.
 `
     }
-  ]
+  ],
+  skills: [
+    ['Comedy', '++++'],
+    ['Love', '++++'],
+    ['Theater', '++++'],
+    ['Tragedy', '++++'],
+    ['Poetry', '++++'],
+    ['Erotic', '+++'],
+    ['Sonnets', '++++'],
+    ['Plays', '++++'],
+    ['Writing', '++++'],
+    ['Directing', '+++'],
+    ['Literature', '++++'],
+    ['Influence', '++++'],
+    ['Influence', '++++'],
+    ['Influence', '++++'],
+    ['Influence', '++++'],
+  ],
 };
