74efe8a6d10cd6ea83b18218df3436477765a952
DeanWinchers <1048402892@qq.com>
2021-08-14 19:55:52 +0800

    add

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -14,37 +14,22 @@ Prince Hamlet is visited by his father's ghost and ordered to avenge his father'
 `
     }
   ],
-  experience: [
-    {
-      header: 'Performance',
-      skills: ['theater', 'play'],
-      contents: `
-Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
-`
-    },
-    {
-      header: 'Poetry',
-      skills: ['poetry', 'erotic'],
-      contents: `
-In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. He dedicated them to Henry Wriothesley, Earl of Southampton.
-`
-    }
-  ],
   skills: [
-    ['Comedy', '++++'],
-    ['Love', '++++'],
-    ['Theater', '++++'],
-    ['Tragedy', '++++'],
-    ['Poetry', '++++'],
-    ['Erotic', '+++'],
-    ['Sonnets', '++++'],
-    ['Plays', '++++'],
-    ['Writing', '++++'],
-    ['Directing', '+++'],
-    ['Literature', '++++'],
-    ['Influence', '++++'],
-    ['Influence', '++++'],
-    ['Influence', '++++'],
-    ['Influence', '++++'],
+    ['C', '++++'],
+    ['C++', '++++'],
+    ['Assembly', '+++'],
+    ['CUDA', '+++'],
+    ['MPI', '+++'],
+    ['pthread', '+++'],
+    ['Makefile', '+++'],
+    ['CMake', '+++'],
+    ['Latex', '++++'],
+    ['Linux', '++++'],
+    ['Git', '++++'],
+    ['Python', '+++'],
+    ['Java', '+++'],
+    ['JavaFX', '++++'],
+    ['Android', '++++'],
+    ['SQL', '++'],
   ],
 };
